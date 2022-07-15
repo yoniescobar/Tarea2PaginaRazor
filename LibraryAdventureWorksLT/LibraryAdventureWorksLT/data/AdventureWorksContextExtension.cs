172ed69e748c79e4825a52a485f3209085246a71
Yoni Edilzar <yoniedilzar@gmail.com>
2022-07-14 20:36:17 -0600

    Creación de Proyecto Incial

--- LibraryAdventureWorksLT/LibraryAdventureWorksLT/data/AdventureWorksContextExtension.cs
+++ LibraryAdventureWorksLT/LibraryAdventureWorksLT/data/AdventureWorksContextExtension.cs
@@ -15,6 +15,7 @@ namespace AdventureWorksNS.data //cambiar con el nombreNS
             this IServiceCollection services,
             string strCnx = "Data Source=MSI;Initial Catalog=AdventureWorksLT2019;Integrated Security=false;User=soporte;Password=12003906;")
         {
+            //Este sera el puente de Blazor
             services.AddDbContext<AdventureWorksDB>(options => options.UseSqlServer(strCnx));
             return services;
         }
