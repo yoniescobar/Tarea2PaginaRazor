3d53d5f88f6c99ac27247674d7cb9787187dfd54
Yoni Edilzar <yoniedilzar@gmail.com>
2022-07-14 22:10:35 -0600

    Add Informacion Add Contactos

---
+++ sitioWebRazor/sitioWebRazor/Pages/informacion.cshtml.cs
@@ -0,0 +1,12 @@
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+
+namespace sitioWebRazor.Pages
+{
+    public class informacionModel : PageModel
+    {
+        public void OnGet()
+        {
+        }
+    }
+}
